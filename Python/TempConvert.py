63dcc87d58044bdf7cbc3f50abc3eaf0ec89daf2
& <1669691816@qq.com>
2020-07-12 13:29:20 +0800

    温度转换小示例

--- Python/TempConvert.py
+++ Python/TempConvert.py
@@ -5,7 +5,7 @@ if TempStr[-1] in ['F', 'f']:
 	C = (eval(TempStr[0:-1]) - 32) / 1.8
 	print("转换后温度的是{:.2f}C".format(C))
 elif TempStr[-1] in ['C', 'c']:
-	C = (eval(TempStr[0:-1]) + 32) * 1.8
+	F = (eval(TempStr[0:-1]) + 32) * 1.8
 	print("转换后温度的是{:.2f}C".format(F))
-else
+else:
 	print("输入格式错误")
